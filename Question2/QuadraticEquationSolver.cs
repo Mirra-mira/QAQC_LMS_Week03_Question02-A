979449ca9497ecaabf72690c227000c53b6e8938
nchinhdat <nchinhdat@DESKTOP-164JDSH>
2025-08-21 19:28:23 +0700

    Add project files.

---
+++ Question2/QuadraticEquationSolver.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+public class QuadraticEquationSolver
+{
+    public static void Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+
+        Console.WriteLine("Nhập hệ số a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhập hệ số b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhập hệ số c:");
+        double c = Convert.ToDouble(Console.ReadLine());
+        string result = Solve(a, b, c);
+        Console.WriteLine(result);
+    }
+    // Phương trình: ax^2 + bx + c = 0
+    public static string Solve(double a, double b, double c)
+    {
+        if (a == 0)
+        {
+            if (b == 0)
+                return (c == 0) ? "Vô số nghiệm" : "Vô nghiệm";
+
+            double x = -c / b;
+            return $"1 nghiệm: x = {x}";
+        }
+
+        double delta = b * b - 4 * a * c;
+
+        if (delta < 0)
+        {
+            return "Vô nghiệm";
+        }
+        else if (delta == 0)
+        {
+            double x = -b / (2 * a);
+            return $"Nghiệm kép: x1 = x2 = {x}";
+        }
+        else
+        {
+            double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+            return $"2 nghiệm: x1 = {x1}, x2 = {x2}";
+        }
+    }
+}
---
+++ Unittest/QuadraticEquationSolverTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class QuadraticEquationSolverTests
+{
+    [TestMethod]
+    public void Test_VoSoNghiem()
+    {
+        string result = QuadraticEquationSolver.Solve(0, 0, 0);
+        Assert.AreEqual("Vô số nghiệm", result);
+    }
+
+    [TestMethod]
+    public void Test_VoNghiem()
+    {
+        string result = QuadraticEquationSolver.Solve(0, 0, 5);
+        Assert.AreEqual("Vô nghiệm", result);
+    }
+
+    [TestMethod]
+    public void Test_PhuongTrinhBacNhat()
+    {
+        string result = QuadraticEquationSolver.Solve(0, 2, -4);
+        Assert.AreEqual("1 nghiệm: x = 2", result);
+    }
+
+    [TestMethod]
+    public void Test_NghiemKep()
+    {
+        string result = QuadraticEquationSolver.Solve(1, 2, 1);
+        Assert.AreEqual("Nghiệm kép: x1 = x2 = -1", result);
+    }
+
+    [TestMethod]
+    public void Test_HaiNghiemPhanBiet()
+    {
+        string result = QuadraticEquationSolver.Solve(1, -3, 2);
+        Assert.AreEqual("2 nghiệm: x1 = 2, x2 = 1", result);
+    }
+}
