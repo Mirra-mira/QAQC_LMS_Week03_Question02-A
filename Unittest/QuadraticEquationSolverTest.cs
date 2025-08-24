20a9fd295493c1822070660cf8c1fccbad5013dc
binhv005 <vtb22522005@gmail.com>
2025-08-24 13:25:19 +0700

    fix unittest

--- Unittest/QuadraticEquationSolverTest.cs
+++ Unittest/QuadraticEquationSolverTest.cs
@@ -1,29 +1,30 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using static QuadraticEquationSolver; 
+using static QuadraticEquationSolver;
 
 namespace Unittest
 {
     [TestClass]
     public sealed class QuadraticEquationSolverTest
     {
+        [TestMethod]
         public void Test_VoNghiem()
         {
             string result = QuadraticEquationSolver.SolveQuadratic(1, 0, 1);
-            Assert.AreEqual("Vô nghiệm", result);
+            Assert.AreEqual("Phương trình vô nghiệm", result);
         }
 
         [TestMethod]
         public void Test_NghiemKep()
         {
             string result = QuadraticEquationSolver.SolveQuadratic(1, 2, 1);
-            Assert.AreEqual("Nghiệm kép: x1 = x2 = -1", result);
+            Assert.AreEqual("Phương trình có nghiệm kép: x1 = x2 = -1", result);
         }
 
         [TestMethod]
         public void Test_HaiNghiem()
         {
             string result = QuadraticEquationSolver.SolveQuadratic(1, -3, 2);
-            Assert.AreEqual("2 nghiệm: x1 = 2, x2 = 1", result);
+            Assert.AreEqual("Phương trình có 2 nghiệm:\nx1 = 2\nx2 = 1", result);
         }
     }
 }
